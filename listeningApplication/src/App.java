a61221c17851f59bfe8e5842050aa6f1b9608c52
reiner-13 <joereiner13@gmail.com>
2025-02-11 21:09:27 -0500

    Created database connection

--- listeningApplication/src/App.java
+++ listeningApplication/src/App.java
@@ -6,7 +6,7 @@ public class App {
         catch (Exception e) {
             e.printStackTrace();
         }
-
+        
         try {
             DatabaseConnectionApp.main(args);
         }
--- listeningApplication/src/DatabaseConnectionApp.java
+++ listeningApplication/src/DatabaseConnectionApp.java
@@ -1,19 +1,46 @@
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseConnectionApp {
+    private Connection con;
     public static void main(String[] args) throws IOException {
-        String url = "jdbc:mysql://localhost:3306;databaseName=MusicApp;encrypt=true;trustServerCertificate=true";
-        String user = "LENXG456/tdavi";
-        String password = "";
+        DatabaseConnectionApp dbca = new DatabaseConnectionApp();
+        dbca.createConnection();
+        dbca.selectUsers();
 
-        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            if (conn != null) {
-                System.out.println("Connected to SQL Server successfully!");
+    }
+
+    private void createConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/listeningapp", "root", "root");
+            
+            System.out.println("Database connection is successful.");
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    private void selectUsers() {
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from users;");
+
+            while (rs.next()) {
+                String username = rs.getString("username");
+                System.out.println(username);
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
     }

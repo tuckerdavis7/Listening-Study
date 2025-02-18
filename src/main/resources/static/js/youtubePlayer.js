899c9504c1c0607496f422e262ef5e4961621fc1
reiner-13 <joereiner13@gmail.com>
2025-02-18 13:31:37 -0500

    Updated YouTube player page

---
+++ src/main/resources/static/js/youtubePlayer.js
@@ -0,0 +1,46 @@
+let player;
+let playing = false;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: 0,
+        width: 0,
+        videoId: '89k1l3-Ruxg',
+        playerVars: {
+            playsinline: 1,
+            autoPlay: 0,
+            controls: 0
+        },
+        events: {
+            onReady: onPlayerReady,
+            onStateChange: onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    // might need later
+}
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING) {
+        playing = true;
+        $('#playButton').html('&#9208;'); // pause symbol
+    }
+    else {
+        playing = false;
+        $('#playButton').html('&#9654;'); // play symbol
+    }
+}
+
+$('#playButton').click(function(event) {
+    if (!playing)
+        player.playVideo()
+    else
+        player.pauseVideo()
+})
+
+$('#volumeRange').change(function(event) {
+    let rangeValue = $(this).val();
+    player.setVolume(rangeValue);
+    $('#volumeLabel').html(`&#128266; ${rangeValue}`);
+})

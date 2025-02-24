09373e7c5002710e7e58ca1a267241f0832297c9
Benjamin Luzier <luzierbenjamin@outlook.com>
2025-02-24 15:03:41 -0500

    Changed from metadata table to view_metaData.

--- src/main/java/com/example/repositories/MetadataRepository.java
+++ src/main/java/com/example/repositories/MetadataRepository.java
@@ -8,7 +8,7 @@ import com.example.DatabaseConnectionApp;
 
 public class MetadataRepository {
     public static ResultSet getApplicationMetadata() throws SQLException {
-        String query = "SELECT * FROM metadata ORDER BY dataID desc LIMIT 1";
+        String query = "SELECT * FROM view_metadata ORDER BY dataID desc LIMIT 1";
         PreparedStatement pstmt = DatabaseConnectionApp.getConnection().prepareStatement(query);
         ResultSet rs = pstmt.executeQuery();
 

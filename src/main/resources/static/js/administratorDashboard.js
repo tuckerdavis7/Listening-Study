e6092b5ce91e382aa313c723be9f259492d03ce9
tuckerdavis7 <tuckjdavis@gmail.com>
2025-03-15 16:19:11 -0400

    More DataTables fixes

--- src/main/resources/static/js/administratorDashboard.js
+++ src/main/resources/static/js/administratorDashboard.js
@@ -10,33 +10,24 @@ var reportData = [
         "modifiedUser": "jdoe" 
     },
     {
-        "initialDate": "03/12/2025 12:29:00",
+        "initialDate": "03/15/2025 09:45:00",
         "first": "John",
         "last": "Cena",
-        "email": "doe@sru.edu",
+        "email": "cena@sru.edu",
         "description": "A long text-field explaining the issue at hand",
         "status": "Open",
-        "modifiedDate": "03/14/2025 13:35:37",
-        "modifiedUser": "reiner_13"
+        "modifiedDate": "03/15/2025 13:50:55",
+        "modifiedUser": "reiner_13" 
     }
-];
+]
 
 $(document).ready(function () {
     reportTable = $('#reportTable').DataTable({
         data: reportData,
-        // ajax:{
-        //     // url: http::/localhost:8080/api/
-        //     // type: 'GET',
-        //     // dataSrc: '',
-        //     // dataType: 'json',
-        //     // data: reportData,
-        //     // timeout: '0',
-        // },
         dom: "<'row'<'col-sm-12 col-md-12 text-end'B>>" +
             "<'row'<'col-sm-12'tr>>" +
             "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>><'#bottomLink'>",
         scrollCollapse: false,
-        scrollY: '50vh',
         responsive: true,
         filter: true,
         info: false,
@@ -49,46 +40,84 @@ $(document).ready(function () {
             {
                 class: "wrenchColumn",
                 data: null,
+                orderable: false,
+                width: "1 em",
                 render: function(data, type, row, meta) {
                     var dropdown = '<div class="dropdown show">' +
                         '<a class="btn-sm btn btn-info" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="fa fa-wrench" aria-hidden="true"></span></a>' +
                         '<div class="dropdown-menu aria-labelledby="dropdownMenuLink">' +
-                        '<a class="dropdown-item" href="#" data-rowindex ="' + meta.row + '" data-bs-toggle="modal" data-bs-target="reportModal">View/Edit Report Details</a>' + 
-                        '<a class="dropdown-item" href="#" data-rowindex ="' + meta.row + '" data-bs-toggle="modal" data-bs-target="resolveModal">Resolve Report</a>' + 
+                        '<a class="dropdown-item" href="#" data-rowindex ="' + meta.row + '" data-bs-toggle="modal" data-bs-target="#reportModal">View/Edit Report Details</a>' + 
+                        '<a class="dropdown-item" href="#" data-rowindex ="' + meta.row + '" data-bs-toggle="modal" data-bs-target="#resolveModal">Resolve Report</a>' + 
                         '</div></div>';
                     return dropdown;
                 },
-                orderable: false,
-                width: "1 em"
             },
             { data: "initialDate", class: "charcolumn", width: "2 rem" },
-            { data: "first", class: "charcolumn", width: "3 rem" },
-            { data: "last", class: "charcolumn", width: "3 rem" },
-            { data: "status", class: "charcolumn", width: "1 rem" },
+            {
+                class: "charColumn",
+                data: null,
+                width: "6 rem",
+                render: function(data, type, row, meta) {
+                    return row.first + ' ' + row.last;
+                }
+            },
+            {
+                class: "charColumn",
+                data: null,
+                width: "6 rem",
+                render: function(data, type, row, meta) {
+                    return row.modifiedDate + ' by ' + row.modifiedUser; 
+                }
+            },
+            {
+                class: "charColumn pillColumn",
+                data: "status",
+                width: "6 rem",
+                render: function(data, type, row, meta) {
+                    let pillClass = (data === "Open") ? "bg-warning" : "bg-success";
+                     return '<span class="badge rounded-pill ' + pillClass + ' text-center d-inline-block">' + data + '</span>';
+                }
+            },
+           
         ],
+        initComplete: function() {
+            //create the search rows
+            this.api().columns().every(function(index) {
+                let column = this;
+                let title = $(column.header()).text().trim();
+                
+                if ($('#reportTable thead tr.filters').length === 0) {
+                    $('#reportTable thead').append('<tr class="filters"></tr>');
+                }
+                
+                if (index === 0) {
+                    $('#reportTable thead tr.filters').append('<th></th>');
+                } 
+                
+                else {
+                    let filterCell = $('<th><input type="text" class="form-control form-control-sm" placeholder="Filter ' + title + '" /></th>');
+                    $('#reportTable thead tr.filters').append(filterCell);
+                    
+                    $('input', filterCell).on('keyup change', function() {
+                        if (column.search() !== this.value) {
+                            column.search(this.value).draw();
+                        }
+                    });
+                }
+            });
+            
+            //disable sorting for search row
+            $('#reportTable thead tr.filters th').addClass('sorting_disabled');
+        },
         drawCallback: function() {
             $('.dt-paging-button.current').attr('style', 'color: white !important'); //inline css styling used as last resort for highest priority selector
         }
     });
 
-    //add a text input to each header cell
-    $('#reportTable thead').append('<tr class="searchRow"></tr>');
-    $('#reportTable').DataTable().columns().every(function (i) {
-        if (i === 0) {
-            $(this.header()).closest('thead').children('.searchRow').append('<th></th>');
-        }
-        else {
-            let that = this;
-            let title = $(this.header()).text();
-            $(this.header()).closest('thead').children('.searchRow').append('<th><input class="form-control form-control-sm m-1 w-100 sorting_disabled" type="search" placeholder="Filter ' + title + '" /></th>');
-            $('input', $('#reportTable thead tr.searchRow th').eq(i)).on('keyup change clear search', function() {
-                if (that.search() !== this.value) {
-                    that.search(this.value).draw();
-                }
-            });
-        }
-    });
-
     //order table by status column
     reportTable.order([[4, 'asc']]).draw();
+
+    $('#reportModal').on('show.bs.modal', function (event) {
+        console.log('hello');
+    });
 })

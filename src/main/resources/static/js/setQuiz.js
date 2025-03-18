fb2f0b2b2ec73c1a70d95bbf6cfa23a425a84b0b
Benjamin Luzier <luzierbenjamin@outlook.com>
2025-03-17 23:54:15 -0400

    Quiz set is ready.

--- src/main/resources/static/js/setQuiz.js
+++ src/main/resources/static/js/setQuiz.js
@@ -1,3 +1,31 @@
+$(document).ready(function() {
+    $('#setButton').click(function(event) {
+        event.preventDefault();
+        let valid = validateFormData();
+
+        if (valid) {
+            //gather form data
+            // let registrationForm = $('#quizSettings').serializeArray().reduce(function (acc, item) {
+            //     acc[item.name] = item.value;
+            //     return acc;
+            // })
+        
+            // //check if serialization maps incorrect key value pairs
+            // if (quizSettings.name && quizSettings.value) {
+            //     registrationForm[registrationForm.name] = registrationForm.value;
+            //     delete quizSettings.name;
+            //     delete quizSettings.value;
+            // }
+
+            //ajax call here once implemented for API
+            bootstrapAlert('success', 'Quiz set!');
+
+            window.location.href='/student/quiz';
+   
+        }
+    });
+});
+
 
 function validateFormData() {
     let allAreFilled = true;

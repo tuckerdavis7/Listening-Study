236f0d0a112f94040771411c9c04ab6621123b76
Benjamin Luzier <luzierbenjamin@outlook.com>
2025-04-21 21:47:20 -0400

    Class Roster Backend Functionality.

---
+++ src/main/java/com/example/repositories/StudentRepository.java
@@ -0,0 +1,20 @@
+package com.example.repositories;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.example.configuration.DatabaseConfiguration;
+
+public class StudentRepository {
+    public ResultSet getStudentRoster(int classID) throws SQLException {
+        String query = "SELECT ID, Email, Firstname, LastName FROM student WHERE classID = ?";
+
+        PreparedStatement pstmt = DatabaseConfiguration.getConnection().prepareStatement(query);
+        pstmt.setInt(1, classID);
+        ResultSet rs = pstmt.executeQuery();
+
+        return rs;
+    }
+    
+}
---
+++ src/main/java/com/example/services/TeacherRosterService.java
@@ -0,0 +1,51 @@
+package com.example.services;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.example.repositories.StudentRepository;
+import com.sun.net.httpserver.HttpExchange;
+
+
+public class TeacherRosterService extends BaseService {
+    private static final Logger logger = LoggerFactory.getLogger(TeacherRosterService.class);
+    private StudentRepository studentRepository = new StudentRepository();
+
+
+    public String getClassRoster(HttpExchange exchange) throws IOException {
+        Map<String, Object> classData = super.getQueryParameters(exchange);
+        int classID = ((Number)classData.get("classID")).intValue();       
+
+        
+        String responseString = "";
+        try {
+            ResultSet result = studentRepository.getStudentRoster(classID);
+            
+            ArrayList<Map<String, Object>> classRoster = new ArrayList<>();
+
+
+            while (result.next()) {
+                Map<String, Object> studentMap = new HashMap<>();
+                studentMap.put("ID", result.getInt("ID"));
+                studentMap.put("Firstname", result.getString("Firstname"));
+                studentMap.put("LastName", result.getInt("LastName"));
+                studentMap.put("Email", result.getInt("Email"));
+                
+                classRoster.add(studentMap);
+            }
+            responseString = super.formatJSON(classRoster, "success");
+        }
+        catch (Exception e) {
+            responseString = "Internal Server Error";
+            logger.error("Error in getClasslist of TeacherRosterService:");
+        }
+        return responseString;
+    }
+    
+}
